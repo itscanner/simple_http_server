a6ad936bf0bbee9fafd13757e31d156e1211cd62
connor <nagela.connor@gmail.com>
2024-04-22 13:39:46 -0500

    server now serves 'index.html'

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 import socket
 
 class HTTP_Server:
@@ -6,28 +7,55 @@ class HTTP_Server:
         s.listen(5)
         print("Server is ready to receive a request")
         
-        while True:
-            (client, address) = s.accept()
-            print(f"Received a connection from {address}")
-            
-            data = client.recv(1024)
-            
-            response = self.handle_request(data)
-            
-            client.sendall(response)
-            client.close()
+        try:
+            while True:
+                (client, address) = s.accept()
+                print(f"Received a connection from {address}")
+                
+                data = client.recv(1024)
+                
+                response = self.handle_request(data)
+                
+                client.sendall(response)
+                client.close()
+        except KeyboardInterrupt:
+            print("Shutting down server.")
+        finally:
+            s.close()
     
-    
-    def init_socket():
-        # Sets up a TCP socket to listen on the HTTP port
-        # param: None
-        # return: The created socket
+    def init_socket(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('', 80))
+        s.bind(('', 80))  # Use 80 for non-root testing
         return s
     
     def handle_request(self, request):
-        # Handle the request and return the response
-        # param: request: The request data
-        # return: The response data
-        pass
+        if request.startswith(b"GET"):
+            return self.handle_get(request)
+        else:
+            return b"HTTP/1.1 501 Not Implemented\r\n\r\n"
+
+    def handle_get(self, request):
+        # Extracting the URI from the request line
+        try:
+            request_line = request.split(b'\r\n')[0]
+            uri = request_line.split(b' ')[1]
+            filename = uri.strip(b'/').decode()
+        except Exception as e:
+            return b"HTTP/1.1 400 Bad Request\r\n\r\n"
+
+        # Handling file response
+        if not os.path.exists(filename):
+            response_line = b"HTTP/1.1 404 Not Found\r\n"
+            response_headers = b"Content-Type: text/html\r\n\r\n"
+            response_body = b"<h1>404 Not Found</h1>"
+        else:
+            response_line = b"HTTP/1.1 200 OK\r\n"
+            response_headers = b"Content-Type: text/html\r\n"
+            response_headers += b"Content-Length: " + str(os.path.getsize(filename)).encode() + b"\r\n\r\n"
+            with open(filename, 'rb') as f:
+                response_body = f.read()
+        
+        return b"".join([response_line, response_headers, response_body])
+
+# Run the server
+HTTP_Server().run_server()
