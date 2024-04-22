3fc0ad1c767cfec95b720fea23aefdfb2593b5d6
connor <nagela.connor@gmail.com>
2024-04-22 14:09:59 -0500

    add documentation

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import socket
 
 class HTTP_Server:
     def run_server(self):
+        # Runs the server and listens for incoming requests
+        # The server will handle the requests and send back the responses
         s = self.init_socket()
         s.listen(5)
         print("Server is ready to receive a request")
@@ -24,17 +26,26 @@ class HTTP_Server:
             s.close()
     
     def init_socket(self):
+        # Initializes the socket
+        # The server will listen on port 80
+        # returns: the socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('', 80))  # Use 80 for non-root testing
         return s
     
     def handle_request(self, request):
+        # Handles the incoming request
+        # request: the incoming request
+        # returns: the response to send back to the client
         if request.startswith(b"GET"):
             return self.handle_get(request)
         else:
             return b"HTTP/1.1 501 Not Implemented\r\n\r\n"
 
     def handle_get(self, request):
+        # Handles the GET request
+        # request: the incoming request
+        # returns: the response to send back to the client
         # Extracting the URI from the request line
         try:
             request_line = request.split(b'\r\n')[0]
